3cdd88270e3f2cfca220c4489ea849e97eccdcfb
Nikitat0 <nikitatzero@yandex.ru>
2023-04-30 11:37:28 +0700

    Prohibit installation if it's already done

--- .github/install.sh
+++ .github/install.sh
@@ -1,2 +1,2 @@
-git clone https://github.com/Nikitat0/dotfiles.git $HOME/.dotfiles --bare
+git clone https://github.com/Nikitat0/dotfiles.git $HOME/.dotfiles --bare &&
 git --work-tree=$HOME --git-dir=$HOME/.dotfiles stash push
